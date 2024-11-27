b7c03d714c11d67028884636f51324f80ec5dcce
Carlos <carlosc72silva@gmail.com>
2024-11-26 21:35:24 -0300

    Subindo projeto

---
+++ tabelacarros.js
@@ -0,0 +1,24 @@
+const carros2024 = [      // armazenamento  
+   { 
+        Nome: 'Ferrari',
+        sigla: 'Fer',
+        velocidadeMaxima: 340,
+        potencia: 800,                              //array
+        consumo: 5.5,
+        acelarador: 2.9,
+        preco: 300000,
+   },    
+   {
+        Nome: 'Lamborghini',
+        sigla: 'LAM',
+        velocidadeMaxima: 355,
+        potencia: 770,                              //array
+        consumo: 6.2,
+        acelarador: 2.8,
+        preco: 330000,
+    },           
+];
+                                            
+ export default carros2024;
+
+
